9b46a5d88352ce6a4ee4684dff8e4f55c0b5c58e
rajakyash <159797605+rajakyash@users.noreply.github.com>
2026-03-13 14:24:45 +0530

    Added secured Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,20 @@
 resource "aws_security_group" "devops_sg" {
-
-  name        = "devops-security-group"
-  description = "Security group for DevOps assignment"
+  name        = "devops-secure-sg"
+  description = "Secure security group for DevOps project"
 
   ingress {
-    description = "SSH access open to internet"
+    description = "Allow SSH from internal network"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]   # INTENTIONAL VULNERABILITY
-  }
-
-  ingress {
-    description = "NodeJS App Port"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
-
+    description = "Allow internal outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
